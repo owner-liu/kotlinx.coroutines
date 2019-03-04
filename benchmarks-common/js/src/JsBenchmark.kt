2f93eeef0bdba025ae260e4276f8f08512f0fb3a
Ilya Ryzhenkov <orangy@jetbrains.com>
2019-03-04 16:36:20 +0300

    ~ Fix benchmarks for js

--- benchmarks-common/js/src/JsBenchmark.kt
+++ benchmarks-common/js/src/JsBenchmark.kt
@@ -1,7 +1,4 @@
-/*
- * Copyright 2016-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
- */
-
+package kotlinx.coroutines.benchmarks
 /*
  * Copyright 2016-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
